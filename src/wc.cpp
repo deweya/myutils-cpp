4e880afdeb2449dfded595c640a94b36ab64d5b2
Austin Dewey <deweya964@gmail.com>
2023-01-08 10:24:34 -0500

    checking if file arg is being passed

--- src/wc.cpp
+++ src/wc.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+const string USAGE = "wc [file]";
+
+struct {
+    string file;
+} opts;
+
+void validateInput(int argc, char** argv) {
+    if (argc == 1) {
+        throw invalid_argument("ERR: argument <file> is required\nUsage: " + USAGE);
+    }
+}
+
 int main(int argc, char** argv) {
-    cout << "Hello world" << endl;
+    try {
+        validateInput(argc, argv);
+    } catch(invalid_argument e) {
+        cerr << e.what() << endl;
+        return -1;
+    }
+
+    opts.file = argv[1];
+    cout << opts.file << endl;
 }
